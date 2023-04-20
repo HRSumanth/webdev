58db86f986dfa1c16352307c736eda77ae4517e4
HRSumanth <130749610+HRSumanth@users.noreply.github.com>
2023-04-20 17:09:30 +0530

    Add files via upload

---
+++ Lets Scale the app to more users/index.js
@@ -0,0 +1,23 @@
+const submit = document.querySelector('#my-form');
+let data=[];
+
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    
+   let myobj = {
+        name:name,
+        email:email
+         }
+
+    data.push(JSON.parse(localStorage.getItem('myobj')));
+    console.log(data)
+    data.push(myobj);
+    localStorage.setItem('myobj',JSON.stringify(data));
+    e.target.name.value= "";
+    e.target.email.value="";
+
+
+})
